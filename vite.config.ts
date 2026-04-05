ed2f680e2cc965134f6e2ba2a9f0a9433a8b1c59
contato187 <contato@ecoglondrina.com.br>
2026-04-05 17:29:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,20 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // Isso diz ao sistema que o símbolo "@" significa a pasta principal
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+  build: {
+    // Garante que o site seja gerado na pasta correta para a Vercel
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+  }
 });
